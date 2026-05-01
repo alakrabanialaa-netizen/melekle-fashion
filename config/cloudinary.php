c39ebf2d333ba4edb8371c561debaeed1f07c3c3
alakrabanialaa-netizen <alakrabanialaa@gmail.com>
2026-05-01 19:43:23 +0300

    Update cloudinary.php

--- config/cloudinary.php
+++ config/cloudinary.php
@@ -1,5 +1,10 @@
 <?php
 
 return [
-    'cloudinary_url' => env('CLOUDINARY_URL'),
+    /*
+    |--------------------------------------------------------------------------
+    | Cloudinary Configuration
+    |--------------------------------------------------------------------------
+    */
+    'cloudinary_url' => 'cloudinary://251326666311568:BP7sMBs-wWEZKHTP3mAmbZkePfQ@doajfaz15',
 ];
