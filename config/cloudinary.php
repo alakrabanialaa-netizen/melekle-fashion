02b12611aa22497069100f3533c69cf25caed062
alakrabanialaa-netizen <alakrabanialaa@gmail.com>
2026-05-03 20:46:47 +0300

    Update cloudinary.php

--- config/cloudinary.php
+++ config/cloudinary.php
@@ -2,4 +2,8 @@
 
 return [
     'cloudinary_url' => 'cloudinary://251326666311568:BP7sMBs-wWEZKHTP3mAmbZkePfQ@doajfaz15',
+    'cloud_name'     => 'doajfaz15',
+    'api_key'        => '251326666311568',
+    'api_secret'     => 'BP7sMBs-wWEZKHTP3mAmbZkePfQ',
+    'upload_preset'  => 'ml_default', // القيمة التي ظهرت في صورتك
 ];
