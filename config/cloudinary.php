1666d5865b6d55836225bef91dfea5a535305116
alakrabanialaa-netizen <alakrabanialaa@gmail.com>
2026-05-03 20:23:33 +0300

    Update cloudinary.php

--- config/cloudinary.php
+++ config/cloudinary.php
@@ -1,4 +1,15 @@
 <?php
+
 return [
-    'cloudinary_url' => env('CLOUDINARY_URL'),
+    /*
+    |--------------------------------------------------------------------------
+    | Cloudinary Configuration
+    |--------------------------------------------------------------------------
+    | سنقوم بوضع الرابط مباشرة هنا لإنهاء مشكلة الـ Null نهائياً
+    */
+    'cloudinary_url' => 'cloudinary://251326666311568:BP7sMBs-wWEZKHTP3mAmbZkePfQ@doajfaz15',
+    
+    'cloud_name' => 'doajfaz15',
+    'api_key'    => '251326666311568',
+    'api_secret' => 'BP7sMBs-wWEZKHTP3mAmbZkePfQ',
 ];
