2a801850bde9900d39c86443de622a2ed4017b86
alakrabanialaa-netizen <alakrabanialaa@gmail.com>
2026-05-11 23:32:22 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,14 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
-    return "الموقع يعمل، يرجى زيارة /run-migrate لإنشاء الجداول.";
+    return "الموقع يعمل، يرجى زيارة /run-migrate لإنشاء الجداول من الصفر.";
 });
 
 Route::get('/run-migrate', function () {
     try {
-        Artisan::call('migrate', ['--force' => true]);
-        return "تم إنشاء الجداول بنجاح:   
+        // هذا الأمر يمسح كل شيء ويبدأ من جديد بالترتيب الصحيح
+        Artisan::call('migrate:fresh', ['--force' => true]);
+        return "تم مسح قاعدة البيانات وإنشاء الجداول بالترتيب الصحيح بنجاح:   
 <pre>" . Artisan::output() . "</pre>";
     } catch (\Exception $e) {
         return "حدث خطأ أثناء إنشاء الجداول: " . $e->getMessage();
