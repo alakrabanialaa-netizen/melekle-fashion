0dda54ba316e8fdcc177666656e1337232a249d3
alakrabanialaa-netizen <alakrabanialaa@gmail.com>
2026-05-16 02:59:17 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -44,7 +44,7 @@ require __DIR__.'/auth.php';
 
 // مجموعة مسارات الآدمن المحمية
 Route::middleware(['auth', 'role:admin'])->group(function() {
-    Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+    Route::get('/admin/dashboard', [AdminDashboardController::class, 'index'])->name('admin.dashboard');
     Route::get('/admin/logout', [AdminController::class, 'AdminDestroy'])->name('admin.logout');
     Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
     Route::post('/admin/profile/store', [AdminController::class, 'AdminProfileStore'])->name('admin.profile.store');
