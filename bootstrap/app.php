16cfd87e75f1fbf6eb931955519aa09cebb1128d
alakrabanialaa-netizen <alakrabanialaa@gmail.com>
2026-05-16 02:16:07 +0300

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,3 +14,12 @@ return Application::configure(basePath: dirname(__DIR__))
         // هذا السطر يحل مشكلة 419 Page Expired للأبد على Render
         $middleware->trustProxies(at: '*');
         
+        // تعريف الـ Aliases للـ Middlewares ليتوافق مع لارافيل 11 ومسارات الـ web.php
+        $middleware->alias([
+            'admin' => \App\Http\Middleware\IsAdmin::class,
+            'role'  => \App\Http\Middleware\Role::class, // تم إضافة هذا السطر لحل مشكلة Target class [role] does not exist
+        ]);
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })->create();
