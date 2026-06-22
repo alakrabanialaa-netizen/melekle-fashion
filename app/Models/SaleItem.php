15900c8d510f661e3bb8d4312d8b5b11f4ea916e
alakrabanialaa-netizen <alakrabanialaa@gmail.com>
2026-06-22 23:01:05 +0300

    Update SaleItem.php

--- app/Models/SaleItem.php
+++ app/Models/SaleItem.php
@@ -2,12 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class SaleItem extends Model
 {
-   public function variant()
-{
-    return $this->belongsTo(ProductVariant::class, 'product_variant_id');
-}
+    use HasFactory;
+
+    // ✅ السماح بحفظ تفاصيل المواد المباعة دون مشاكل Mass Assignment
+    protected $fillable = [
+        'sale_id',
+        'product_id',
+        'product_variant_id',
+        'quantity',
+        'price',
+        'profit',
+    ];
 }
