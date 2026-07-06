f48b8fe16e272a4bf2c5db9d2de421f26f98682a
alakrabanialaa-netizen <alakrabanialaa@gmail.com>
2026-07-06 23:56:37 +0300

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,22 +8,21 @@ use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
-    public function run(): void
-    {
-        // الأدمن الأول (القديم)
-        User::create([
-            'name' => 'Alaa',
-            'email' => 'alaa@example.com',
-            'password' => Hash::make('123456'),
-            'is_admin' => 1,
-        ]);
+  public function run(): void
+{
+    // الأدمن الأول (حسابك)
+    \App\Models\User::create([
+        'name' => 'Alaa',
+        'email' => 'alaa@example.com',
+        'password' => \Illuminate\Support\Facades\Hash::make('123456'),
+        'is_admin' => 1,
+    ]);
 
-        // الأدمن الجديد
-        User::create([
-            'name' => 'New Admin', 
-            'email' => 'newadmin@example.com',
-            'password' => Hash::make('password123'),
-            'is_admin' => 1,
-        ]);
-    }
+    // الأدمن الجديد 
+    \App\Models\User::create([
+        'name' => 'New Admin', 
+        'email' => 'newadmin@example.com',
+        'password' => \Illuminate\Support\Facades\Hash::make('password123'),
+        'is_admin' => 1,
+    ]);
 }
